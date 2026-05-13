4e484b3fda47a845e8768bdd9fcec1a47d9b90b0
Mihara-Yamato-com <yamato-mihara@comthink.co.jp>
2026-05-13 17:04:04 +0900

    初期版提出

---
+++ db/migrate/20260513054935_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.2]
+  def change
+    create_table :tasks do |t|
+      t.string  :title,       null: false
+      t.text    :description
+      t.integer :priority,    null: false, default: 1  # 1:低 2:中 3:高
+      t.date    :due_date
+      t.boolean :done,        null: false, default: false
+      t.timestamps
+    end
+  end
+end
