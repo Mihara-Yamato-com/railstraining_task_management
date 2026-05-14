53bfff881ebad7d3d3664b51fb9fe98c38626c61
Mihara-Yamato-com <yamato-mihara@comthink.co.jp>
2026-05-14 15:27:37 +0900

    修正版

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
   # バリデーション
   validates :title, presence: true, length: { maximum: 100 }
-  validates :priority, inclusion: { in: [1, 2, 3] }
-  validates :done, inclusion: { in: [true, false] }
+  validates :priority, inclusion: { in: [ 1, 2, 3 ] }
+  validates :done, inclusion: { in: [ true, false ] }
 
   # 優先度を文字列で返すメソッド
   PRIORITY_LABELS = { 1 => "低", 2 => "中", 3 => "高" }.freeze
